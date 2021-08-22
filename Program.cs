f210c9403ac2f443ae0a64741ffac921101781c6
Vadim <bbrb2110@gmail.com>
2021-08-22 20:53:00 +0300

    Adddg  fdhghdhdg

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace HelloDay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello dfgfgdegfdfgdfgdf");
+
         }
     }
 }
